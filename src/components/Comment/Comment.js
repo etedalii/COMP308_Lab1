8098327fe2a6bb1fe009c09f90a86f718ebb5dd3
etedali <etedali@gmail.com>
2022-02-04 01:31:19 -0500

    added event for comment

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,50 +1,71 @@
-import React, { useState, useEffect }  from 'react';
-import auth  from '../../auth/auth';
-import './Comment.css';
+import React, { useState, useEffect } from "react";
+import auth from "../../auth/auth";
+import "./Comment.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export default function Comment(props) {
-
- 
   const [comment, setComment] = useState();
   const [email, setEmail] = useState();
- // let loggedEmail = auth.getToken();
+  const [courseCode, setCourseCode] = useState();
+  const [courseName, setCourseName] = useState();
+  const [semester, setSemester] = useState();
+  const [section, setSection] = useState();
 
   useEffect(() => {
     const loggedInUser = auth.getToken();
-   
-  //  setEmail(loggedInUser);
-   if (loggedInUser!=="") {
-        setEmail(loggedInUser);
+
+    if (loggedInUser !== "") {
+      setEmail(loggedInUser);
     }
-},[]);
- 
-  const handleSubmit = e => {
+  }, []);
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-   // auth.onAuthentication();
-    props.history.push('/result');
- 
-    
+    // auth.onAuthentication();
+    props.history.push("/result");
   };
-  
-    return(
-       
-        <div className="comment-wrapper">
-      <h1>Enter Your Comments</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <p>Email</p>
-          <input type="text" defaultValue ={email} disabled = {true}/>
-        </label>
-        <label>
-          <p>Comment</p>
-          <textarea onChange={e => setComment(e.target.value)} rows={5}/>
-        </label>
-       
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-      </div>
-    )
-  }
-  
+
+  return (
+    <div className="Comment">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <h4>Enter Your Comments</h4>
+        </Form.Group>
+
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="text" defaultValue={email} disabled={true} />
+        </Form.Group>
+
+        <Form.Group size="lg" controlId="courseCode">
+          <Form.Label>Course Code</Form.Label>
+          <Form.Control type="text" onChange={e => setCourseCode(e.target.value)} />
+        </Form.Group>
+
+        <Form.Group size="lg" controlId="courseName">
+          <Form.Label>Course Name</Form.Label>
+          <Form.Control type="text" onChange={e => setCourseName(e.target.value)} />
+        </Form.Group>
+
+        <Form.Group size="lg" controlId="section">
+          <Form.Label>Section</Form.Label>
+          <Form.Control type="text" onChange={e => setSection(e.target.value)} />
+        </Form.Group>
+        <Form.Group size="lg" controlId="semester">
+          <Form.Label>Semester</Form.Label>
+          <Form.Control type="text" onChange={e => setSemester(e.target.value)} />
+        </Form.Group>
+
+        <Form.Group size="lg" controlId="comment">
+          <Form.Label>Comment</Form.Label>
+          <Form.Control type="text"></Form.Control>
+        </Form.Group>
+
+        <Button block size="lg" className="mt-2" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
