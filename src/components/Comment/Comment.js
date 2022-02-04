3f20e94faea7c36c67f3ce2dfd4664f2ebfe023d
etedali <etedali@gmail.com>
2022-02-03 19:45:17 -0500

    added the login page

---
+++ src/components/Comment/Comment.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect }  from 'react';
+import auth  from '../../auth/auth';
+import './Comment.css';
+
+export default function Comment(props) {
+
+ 
+  const [comment, setComment] = useState();
+  const [email, setEmail] = useState();
+ // let loggedEmail = auth.getToken();
+
+  useEffect(() => {
+    const loggedInUser = auth.getToken();
+   
+  //  setEmail(loggedInUser);
+   if (loggedInUser!=="") {
+        setEmail(loggedInUser);
+    }
+},[]);
+ 
+  const handleSubmit = e => {
+    e.preventDefault();
+   // auth.onAuthentication();
+    props.history.push('/result');
+ 
+    
+  };
+  
+    return(
+       
+        <div className="comment-wrapper">
+      <h1>Enter Your Comments</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Email</p>
+          <input type="text" defaultValue ={email} disabled = {true}/>
+        </label>
+        <label>
+          <p>Comment</p>
+          <textarea onChange={e => setComment(e.target.value)} rows={5}/>
+        </label>
+       
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      </div>
+    )
+  }
+  
---
+++ src/components/Login/Login.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import auth from "../../auth/auth";
+import "./Login.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+/*
+    Mohammad Etedali 301056465
+    COMP 308 - Lab 1
+*/
+export default function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    auth.onAuthentication();
+    auth.saveToken(email);
+    props.history.push("/comment");
+  }
+
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button block size="lg" className="mt-2" type="submit" disabled={!validateForm()}>
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
+}
